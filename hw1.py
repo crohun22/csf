701247ac3a645595d1d77de5d2656f6d81fef000
crohun22 <crohun22@ccm-2610-d2.evergreen.edu>
2013-10-03 16:41:24 -0700

    finished all 3 parts

--- hw1.py
+++ hw1.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Hunter Crook
+# Evergreen Login: crohun22
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 1
@@ -18,16 +18,32 @@ import math                     # makes the math.sqrt function available
 
 print "Problem 1 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+A = 1
+B = -5.86
+C = 8.5408
 
 
+X1 = (-B + math.sqrt((B*B) - (4*A*C))) / (2*A)
+
+X2 = (-B - math.sqrt((B*B) - (4*A*C))) / (2*A)
+
+print "For X when A = " + str(A) + " when B = " + str(B) + " and when C = " + str(C)
+print str(X1)
+print str(X2)
 ###
 ### Problem 2
 ###
 
 print "Problem 2 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+from hw1_test import a, b, c, d, e, f
+
+print str(a)
+print str(b)
+print str(c)
+print str(d)
+print str(e)
+print str(f)
 
 
 ###
@@ -36,9 +52,9 @@ print "Problem 2 solution follows:"
 
 print "Problem 3 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+z = ((a * b) - (-c) * -(d - e - f))
 
-
+print str(z)
 ###
 ### Collaboration
 ###
