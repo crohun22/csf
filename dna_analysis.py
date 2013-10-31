b5bc2cc924536ef3d1e08a9348fcd73c3d0cb973
crohun22 <crohun22@ccm-2610-e1.evergreen.edu>
2013-10-31 16:08:39 -0700

    finished problem 5

--- dna_analysis.py
+++ dna_analysis.py
@@ -51,17 +51,26 @@ for line in inputfile:
 ###
 
 # Total nucleotides seen so far.
-total_count = 0
+gcTotal_count = 0
 atTotal_count = 0
-# Number of G and C nucleotides seen so far.
+aTotal_count = 0
+tTotal_count = 0
+cTotal_count = 0
+gTotal_count = 0
+
+# Number of nucleotides seen so far.
 gc_count = 0
 at_count = 0
+a_count = 0
+t_count = 0
+c_count = 0
+g_count = 0
 
 
 # for each base pair in the string,
 for bp in seq:
     # increment the total number of bps we've seen
-    total_count = total_count + 1
+    gcTotal_count = gcTotal_count + 1
 
     # next, if the bp is a G or a C,
     if bp == 'C' or bp == 'G':
@@ -78,12 +87,52 @@ for bp in seq:
         # increment the count of gc
         at_count = at_count + 1
 
+# for each each seperate nucleotide in the string,
+for bp in seq:
+    # increment the total number of bps we've seen
+    aTotal_count = aTotal_count + 1
+    tTotal_count = tTotal_count + 1
+    cTotal_count = cTotal_count + 1
+    gTotal_count = gTotal_count + 1
+
+    # next, if the bp is a G or a C,
+    if bp == 'A':
+        # increment the count of gc
+        a_count = a_count + 1
+    elif bp == 'T':
+        # increment the count of gc
+        t_count = t_count + 1
+    elif bp == 'C':
+        # increment the count of gc
+        c_count = c_count + 1
+    elif bp == 'G':
+        # increment the count of gc
+        g_count = g_count + 1
+
+
 # divide the gc_count by the total_count
-gc_content = float(gc_count) / total_count
+gc_content = float(gc_count) / gcTotal_count
 
 # divide the at_count by the atTotal_count
 at_content = float(at_count) / atTotal_count
 
+# divide the a_count by the aTotal_count
+a_content = float(a_count) / aTotal_count
+
+# divide the t_count by the tTotal_count
+t_content = float(t_count) / tTotal_count
+
+# divide the c_count by the cTotal_count
+c_content = float(c_count) / cTotal_count
+
+# divide the g_count by the gTotal_count
+g_content = float(g_count) / gTotal_count
+
+
 # Print the answer
 print 'GC-content:', gc_content
 print 'AT-content:', at_content
+print 'A count:', a_content
+print 'T count:', t_content
+print 'G count:', g_content
+print 'C count:', c_content
